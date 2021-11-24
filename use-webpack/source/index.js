2a842537f08b16df86bc53fc0dfe6353de8768ee
hyoni0817 <dudu1104@naver.com>
2021-11-24 20:47:01 +0900

    Feat: lodash를 추가하여 Hello world 구현

--- use-webpack/source/index.js
+++ use-webpack/source/index.js
@@ -1,6 +1,12 @@
 import hello_word from "./hello.js";
 import world_word from "./world.js";
+import _ from "lodash";
 import css from "./style.css";
-document.querySelector("#root").innerHTML = hello_word + " " + world_word;
+
+document.querySelector("#root").innerHTML = _.join([
+  hello_word,
+  world_word,
+  " ",
+]);
 
 console.log("css", css); //css-loader만 사용했을 때 값이 출력됨.
